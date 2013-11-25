210a71d6f9f679edbcfc45616294830f3a7f064c
Ayesha Bose <aybose@mit.edu>
2013-11-25 14:19:15 -0500

    Added PCA page and dummy viz

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='dashboard/index.html')),
 	url(r'granger/$', TemplateView.as_view(template_name='dashboard/granger.html')),
 	url(r'about/$', TemplateView.as_view(template_name='dashboard/about.html')),
+	url(r'pca/$', TemplateView.as_view(template_name='dashboard/pca.html')),
 )
