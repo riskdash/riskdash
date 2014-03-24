65f663d64b7a4e4bac10bb2045a6f8bd1cc7eb55
Ayesha Bose <aybose@mit.edu>
2014-03-24 15:15:51 -0700

    Fixed other pages, added contact page

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='dashboard/index.html')),
 	url(r'granger/$', TemplateView.as_view(template_name='dashboard/granger.html')),
 	url(r'about/$', TemplateView.as_view(template_name='dashboard/about.html')),
+	url(r'contact/$', TemplateView.as_view(template_name='dashboard/contact.html')),
 	url(r'pca/$', TemplateView.as_view(template_name='dashboard/pca.html')),
 	url(r'returns/$', TemplateView.as_view(template_name='dashboard/returns.html')),
 )
