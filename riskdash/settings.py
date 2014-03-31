a7f2817648b2270175581a491de0260ae1d4320a
Ayesha Bose <aybose@mit.edu>
2014-03-31 12:31:13 -0400

    Set up Postgres db

--- riskdash/settings.py
+++ riskdash/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'riskdash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'riskdash',
+        'USER': 'riskdash',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -71,3 +75,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if os.environ.get('PRODUCTION') == 'True':
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
