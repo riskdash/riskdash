1097ac43712aca811cd39fbd888b79b7f1cf7800
Ayesha Bose <aybose@mit.edu>
2014-04-15 01:43:43 -0400

    Changed scale on CRF graph

--- apps/dashboard/static/js/pca_viz.js
+++ apps/dashboard/static/js/pca_viz.js
@@ -56,7 +56,7 @@ d3.json("/pca/data/", function(error, data) {
   });
 
   x.domain(d3.extent(data.map(function(d) { return d.date; })));
-  y.domain([0, d3.max(data.map(function(d) { return d.price; }))]);
+  y.domain([0.6, d3.max(data.map(function(d) { return d.price; }))]);
   x2.domain(x.domain());
   y2.domain(y.domain());
 
