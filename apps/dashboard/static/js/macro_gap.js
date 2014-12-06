40037f0bf081c5b829256af3b0a84aa7b68a0232
Ray Weng <rw422@cornell.edu>
2014-12-06 00:33:49 -0800

    created page for macroeconomic gaps

--- apps/dashboard/static/js/macro_gap.js
+++ apps/dashboard/static/js/macro_gap.js
@@ -1,26 +1,82 @@
+min_width = '400px'
+
+// look at https://gist.github.com/femto113/1784503 to find out how to transpose data arrays so that we can parse a csv with multiple countries' data
+
 d3.text('/static/data/credit_gap.csv', function(unparsedData) {
 	var data = d3.csv.parse(unparsedData, function(d) {
-		
-		var pattern = /(\d{4})(\d{2})(\d{2})/;
-		return [new Date(d.date.replace(pattern,'$1-$2-$3')), +d.noise];
-
+		return [+d.year, +d.credit_gap];
 	});
 
 	// Create the chart
-	$('#container').highcharts('StockChart', {
+	$('#credit_gap').highcharts({
+		title: {
+			text: 'Credit/GDP Gap',
+		},
 
-
-		rangeSelector : {
-			selected : 5,
-			inputEnabled: $('#container').width() > 480
+		legend: {
+			enabled: false, // enable it when there are more lines
 		},
 
 		series : [{
-			name : 'Noise',
+			name : 'Credit Gap',
 			data : data,
 			tooltip: {
-				valueDecimals: 4
+				valueDecimals: 2
+			}
+		}]
+	});
+
+});
+
+d3.text('/static/data/property_gap.csv', function(unparsedData) {
+	var data = d3.csv.parse(unparsedData, function(d) {
+		return [+d.year, +d.property_gap];
+	});
+
+	// Create the chart
+	$('#property_gap').highcharts({
+		title: {
+			text: 'Real Property Price Gap',
+		},
+
+		legend: {
+			enabled: false, // enable it when there are more lines
+		},
+
+		series : [{
+			name : 'Property Gap',
+			data : data,
+			tooltip: {
+				valueDecimals: 2
 			}
 		}]
 	});
 });
+
+d3.text('/static/data/market_gap.csv', function(unparsedData) {
+	var data = d3.csv.parse(unparsedData, function(d) {
+		return [+d.year, +d.market_gap];
+	});
+
+	// Create the chart
+	$('#market_gap').highcharts({
+		title: {
+			text: 'Market (Equity) Gap',
+		},
+
+		legend: {
+			enabled: false, // enable it when there are more lines
+		},
+
+		series : [{
+			name : 'Market Gap',
+			data : data,
+			tooltip: {
+				valueDecimals: 2
+			}
+		}]
+	});
+});
+
+// set all the graphs to minimum width
+d3.select('#container').selectAll('span').style('min-width',min_width).style('float','left');
